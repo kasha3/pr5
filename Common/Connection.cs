c07d0ded92ac3b4c25db9e90460146aea67edcbe
FadAlex <fadeevsasha0906@gmail.com>
2024-12-16 18:10:37 +0500

    Классы пользователя и подключения

---
+++ Common/Connection.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Common
+{
+    public class Connection
+    {
+        public TcpClient Client;
+        public User User { get; set; }
+        public Guid Token { get; set; } = Guid.NewGuid();
+        private Timer Timer;
+        public DateTime ConnectionTime { get; set; } = DateTime.Now;
+        public Connection(TcpClient client, int disconnectTime) 
+        {
+            Client = client;
+            Timer = new Timer();
+            Timer.Interval = TimeSpan.FromSeconds(disconnectTime).TotalMilliseconds;
+            Timer.AutoReset = false;
+            Timer.Elapsed += OnTimerElapsed;
+            Timer.Start();
+        }
+
+        private void OnTimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            Disconnect();
+        }
+
+        public void Disconnect()
+        {
+            Timer.Stop();
+            Client.Close();
+        }
+
+        public override string ToString()
+        {
+            return $"Дата и время: {ConnectionTime}, Время подключения:{DateTime.Now - ConnectionTime}, Адрес и порт: {IPAddress.Parse(((IPEndPoint)Client.Client.RemoteEndPoint).Address.ToString())}:{((IPEndPoint)Client.Client.RemoteEndPoint).Port.ToString()}";
+        }
+    }
+}
---
+++ Common/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class User
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Password { get; set; }
+        public string Login { get; set; }
+        public bool IsBlocked { get; set; } = false;
+    }
+}
