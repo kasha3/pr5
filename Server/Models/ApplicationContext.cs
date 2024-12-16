5a77d2685ab5d6dff34fe43797caf48058630d21
FadAlex <fadeevsasha0906@gmail.com>
2024-12-16 19:04:06 +0500

    Добавлен контекст

---
+++ Server/Models/ApplicationContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Common.User> Users { get; set; }
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(@"server=127.0.0.1;port=3307;database=pr5;uid=root;pwd=;");
+        }
+    }
+}
